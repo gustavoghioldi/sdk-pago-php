b8e8225f22046faddaeb4349dfa2b4506bb8aaef
gustavoghioldi <gustavoghioldi@gmail.com>
2014-09-02 17:05:14 -0300

    anda!!!!!

--- ejemplo/form.php
+++ ejemplo/form.php
@@ -1,66 +1,69 @@
 <?php
-// include_once '../lib/Decidir.php';
+include_once '../lib/Decidir.php';
 
-// $options = array (
-// 'nrocomercio' => 'NRO_COMERCIO',
-// 'nrooperacion' => 'NRO_OPERACION',
-// 'monto' => 'MONTO',
-// 'cuotas' => 'CUOTAS',
-// 'mediodepago' => 'MEDIO_DE_PAGO',
-// 'emailcliente' => 'EM@AIL.COM',
-// 'paramsitio' => 'PARAM_SITIO',
-// 'impdist' => '',
-// 'sitedist' => '',
-// 'idmodalidad' => '',
-// 'porcentaje' => '',
-// 'nrodoc' => '',
-// 'calleentrega' => '',
-// 'nropuerta' => '',
-// 'fechanacimiento' => '',
-// 'codp1' => '',
-// 'codp2' => '',
-// 'codp3' => '',
-// 'codp4' => '',
-// 'recargo' => '',
-// 'fechavto' => '',
-// 'cliente' => '',
-// 'titular' => '',
-// 'tipotoc' => '',
-// 'nrodoc' => '',
-// 'fechavto' => '',
-// 'idplan' => '',
-// 'vtex1x2x3x4' => '',
-// 'url_ok' => '',
-// 'url_error' => '',
-// 'encodingmethod' => 'AHHHH'
-// );
-ini_set ( 'default_socket_timeout', 1200 );
-ini_set ( 'max_execution_time', 1000 ); // just put a lot of time
-ini_set ( 'default_socket_timeout', 1000 ); // same
-ini_set ( 'soap.wsdl_cache_enabled', 0 );
+$options = array (
+'nrocomercio' => 'NRO_COMERCIO',
+'nrooperacion' => 'NRO_OPERACION',
+'monto' => 'MONTO',
+'cuotas' => 'CUOTAS',
+'mediodepago' => 'MEDIO_DE_PAGO',
+'emailcliente' => 'EM@AIL.COM',
+'paramsitio' => 'PARAM_SITIO',
+'impdist' => '',
+'sitedist' => '',
+'idmodalidad' => '',
+'porcentaje' => '',
+'nrodoc' => '',
+'calleentrega' => '',
+'nropuerta' => '',
+'fechanacimiento' => '',
+'codp1' => '',
+'codp2' => '',
+'codp3' => '',
+'codp4' => '',
+'recargo' => '',
+'fechavto' => '',
+'cliente' => '',
+'titular' => '',
+'tipotoc' => '',
+'nrodoc' => '',
+'fechavto' => '',
+'idplan' => '',
+'vtex1x2x3x4' => '',
+'url_ok' => '',
+'url_error' => '',
+'encodingmethod' => 'AHHHH'
+);
+// ini_set ( 'default_socket_timeout', 1200 );
+// ini_set ( 'max_execution_time', 1000 ); // just put a lot of time
+// ini_set ( 'default_socket_timeout', 1000 ); // same
+// ini_set ( 'soap.wsdl_cache_enabled', 0 );
 
-$localCert = ("http://127.0.0.1/sdk-pago-php/ejemplo/certificado.pem");
-$wsdl = 'Authorize.wsdl';
-$client = new SoapClient ( $wsdl, array (
-		"connection_timeout" => 1000,
-		'local_cert' => $localCert,
-		'style' => SOAP_DOCUMENT,
-		'use' => SOAP_LITERAL,
-		'location' => 'https://200.69.248.51:8443/services/t/decidir.net/Authorize',
-		'encoding' => 'UTF-8' 
-// 'proxy_host' => '192.168.0.17',
-// 'proxy_port' => 8080
+// $localCert = ("http://127.0.0.1/sdk-pago-php/ejemplo/certificado.pem");
+// $wsdl = 'Authorize.wsdl';
+// $client = new SoapClient ( $wsdl, array (
+// 		"connection_timeout" => 1000,
+// 		'local_cert' => $localCert,
+// 		'style' => SOAP_DOCUMENT,
+// 		'use' => SOAP_LITERAL,
+// 		'location' => 'https://200.69.248.51:8443/services/t/decidir.net/Authorize',
+// 		'encoding' => 'UTF-8', 
+// 		'proxy_host' => '192.168.0.17',
+// 		'proxy_port' => 8080
 // // 'proxy_login' => 'gghioldi',
 // // 'proxy_password' => '1270pesecin'
-) );
+// ) );
 
-var_dump ( $client->AuthorizeRequest ( array (
-		'SessionID' => '025',
-		'EncodingMethod' => 'UTF-8',
-		'URL_ERROR' => 'http://www.google.com',
-		'URL_OK' => 'http://www.yahoo.com.ar',
-		'Payload' => '<cosas>lala</cosas>' 
-)
- ) );
+// var_dump ( $client->AuthorizeRequest ( array (
+// 		'SessionID' => '025',
+// 		'EncodingMethod' => 'UTF-8',
+// 		'URL_ERROR' => 'http://www.google.com',
+// 		'URL_OK' => 'http://www.yahoo.com.ar',
+// 		'Payload' => '<cosas>lala</cosas>' 
+// )
+//  ) );
 
+$client = new DecidirConnector();
+$values = $client->getPaymentValues($options);
+header("Location:".$values['URL_Request']);
 ?>
--- lib/Decidir.php
+++ lib/Decidir.php
@@ -1,88 +1,86 @@
 <?php
 define ( 'VERSION', '0.1.0-GUS' );
-define ( 'END_POINT', "http://200.69.248.51:8443/services/t/decidir.net/Authorize.AuthorizeHttpsSoap12Endpoint?wsdl" );
-define ( 'OLD_END_POINT', "http://api.decidir.com.ar/services/Authorize" );
+define ( 'WSDL', "Authorize.wsdl" );
+define ( 'END_POINT', "https://200.69.248.51:8443/services/t/decidir.net/Authorize" );
 define ( 'ENCODING_METHOD', "UTF-8" );
-
+define ( 'CERT', "certificado.pem");
 class DecidirConnector {
 	
 	/*
 	 * GET_PAYMENT_VALUES
-	* */
-	
+	 */
 	public function getPaymentValues($options) {
-		//parseo de los valores enviados por el e-commerce/custompage par aformar el obj autorizereuest
-		$authorizeRequest = $this->parseToAuthorizeRequest($options);
+		// parseo de los valores enviados por el e-commerce/custompage par aformar el obj autorizereuest
+		$authorizeRequest = $this->parseToAuthorizeRequest ( $options );
 		
-		//me lo devuelve desde el gate way, url , y unas coas mas (ojo muy importanes!!)
-		$authorizeRequestResponse = $this->getAuthorizeRequestResponse($authorizeRequest);
+		// me lo devuelve desde el gate way, url , y unas coas mas (ojo muy importanes!!)
+		$authorizeRequestResponse = $this->getAuthorizeRequestResponse ( $authorizeRequest );
 		
-		//me devuleve el array que me dio el gw
-		$authorizeRequestResponseValues = $this->parseAuthorizeRequestResponseToArray($authorizeRequestResponse);
+		// me devuleve el array que me dio el gw
+		$authorizeRequestResponseValues = $this->parseAuthorizeRequestResponseToArray ( $authorizeRequestResponse );
 		
 		return $authorizeRequestResponseValues;
 	}
-	
 	private function parseToAuthorizeRequest($options) {
-		$authorizeRequest = new stdClass();
-		$authorizeRequest->Payload = $this->getPayload ($options);
-		$authorizeRequest->URL_OK = $options['url_ok'];
-		$authorizeRequest->URL_ERROR = $options['url_error'];
-		$authorizeRequest->SessionID = $options['nrooperacion'];
-		$authorizeRequest->EncodingMethod = $options['encodingmethod'];
+		$authorizeRequest = new stdClass ();
+		$authorizeRequest->Payload = $this->getPayload ( $options );
+		$authorizeRequest->URL_OK = $options ['url_ok'];
+		$authorizeRequest->URL_ERROR = $options ['url_error'];
+		$authorizeRequest->SessionID = $options ['nrooperacion'];
+		$authorizeRequest->EncodingMethod = $options ['encodingmethod'];
 		
 		return $authorizeRequest;
 	}
 	
-	//ver esta funcoion que es la que me va a trater el token
-	private function getAuthorizeRequestResponse($authorizeRequest)
-	{
-		$clientSoap = new SoapClient(END_POINT);
-		$authorizeRequestResponse = $clientSoap->AuthorizeRequest($authorizeRequest);
+	// ver esta funcoion que es la que me va a trater el token
+	private function getAuthorizeRequestResponse($authorizeRequest) {
+		$clientSoap = new SoapClient ( WSDL, array (
+				"connection_timeout" => 1000,
+				'local_cert' => CERT,
+				'style' => SOAP_DOCUMENT,
+				'use' => SOAP_LITERAL,
+				'location' => 'https://200.69.248.51:8443/services/t/decidir.net/Authorize',
+				'encoding' => 'UTF-8',
+				'proxy_host' => '192.168.0.17',
+				'proxy_port' => 8080 
+		// // 'proxy_login' => 'gghioldi',
+		// // 'proxy_password' => '1270pesecin'
+				) );
+		$authorizeRequestResponse = $clientSoap->AuthorizeRequest ( $authorizeRequest );
 		return $authorizeRequestResponse;
-
-		return $authorizeRequest;
 	}
-	
 	private function parseAuthorizeRequestResponseToArray($authorizeRequestResponse) {
-		
-		$authorizeRequestResponseOptions = $array = json_decode(json_encode($authorizeRequestResponse), true);
+		$authorizeRequestResponseOptions = $array = json_decode ( json_encode ( $authorizeRequestResponse ), true );
 		
 		return $authorizeRequestResponseOptions;
 	}
 	
-	//ver metodos del api de php para hacer esto
+	// ver metodos del api de php para hacer esto
 	private function getPayload($options) {
-		$xmlPayload = "";		
-		foreach ( $options as $key => $value ) {			
+		$xmlPayload = "";
+		foreach ( $options as $key => $value ) {
 			$xmlPayload .= "<" . $key . ">" . $value . "</" . $key . ">";
 		}
 		
 		return $xmlPayload;
 	}
 	
-	
-	
 	/*
 	 * QUERY_PAYMENT
-	 * */
-	
+	 */
 	public function queryPayment($SessionID, $RequestKey, $AnswerKey) {
+		$authorizeAnswer = $this->parseToAuthorizeAnswer ( $SessionID, $RequestKey, $AnswerKey );
 		
-		$authorizeAnswer = $this->parseToAuthorizeAnswer($SessionID, $RequestKey, $AnswerKey);
+		// me lo devuelve desde el gate way, url , y unas coas mas (ojo muy importanes!!)
+		$authorizeAnswerResponse = $this->getAuthorizeAnswerResponse ( $authorizeAnswer );
 		
-		//me lo devuelve desde el gate way, url , y unas coas mas (ojo muy importanes!!)
-		$authorizeAnswerResponse = $this->getAuthorizeAnswerResponse($authorizeAnswer);
-		
-		//me devuleve el array que me dio el gw
-		$authorizeAnswerResponseValues = $this->parseAuthorizeAnswerResponseToArray($authorizeAnswerResponse);
+		// me devuleve el array que me dio el gw
+		$authorizeAnswerResponseValues = $this->parseAuthorizeAnswerResponseToArray ( $authorizeAnswerResponse );
 		
 		return $authorizeAnswerResponseValues;
 	}
-	
 	private function parseToAuthorizeAnswer($SessionID, $RequestKey, $AnswerKey) {
-		
-		$authorizeAnswer = new stdClass();
+		$authorizeAnswer = new stdClass ();
 		
 		$authorizeAnswer->SessionID = $SessionID;
 		$authorizeAnswer->RequestKey = $RequestKey;
@@ -90,17 +88,13 @@ class DecidirConnector {
 		
 		return $authorizeAnswer;
 	}
-	
 	private function getAuthorizeAnswerResponse($authorizeAnswer) {
-// 		$clientSoapAuthorizeAnswer = SoapClient (END_POINT);
-// 		$authorizeAnswerResponse = $clientSoap->AuthorizeAnswer($authorizeAnswer);
-		
+		// $clientSoapAuthorizeAnswer = SoapClient (END_POINT);
+		// $authorizeAnswerResponse = $clientSoap->AuthorizeAnswer($authorizeAnswer);
 		return $authorizeAnswer;
 	}
-	
 	private function parseAuthorizeAnswerResponseToArray($authorizeAnswerResponse) {
-		
-		$authorizeAnswerResponseOptions = $array = json_decode(json_encode($authorizeAnswerResponse), true);
+		$authorizeAnswerResponseOptions = $array = json_decode ( json_encode ( $authorizeAnswerResponse ), true );
 		
 		return $authorizeAnswerResponseOptions;
 	}
